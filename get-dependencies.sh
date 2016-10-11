3bfcb83543ff42d5645491ed559f4c93731f759b
juju4 <juju4@users.noreply.github.com>
2016-10-10 23:38:17 -0400

    galaxy naming for dependencies

---
+++ get-dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+## one script to be used by travis, jenkins, packer...
+
+umask 022
+
+if [ $# != 0 ]; then
+rolesdir=$1
+else
+rolesdir=$(dirname $0)/..
+fi
+
+[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+
