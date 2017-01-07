9657f474b183ec8e7c4d2cc9a5dbd03ab46e2463
juju4 <juju4@users.noreply.github.com>
2017-01-07 05:32:44 -0500

    sync repositories - add Jenkinsfile

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,4 +10,10 @@ rolesdir=$(dirname $0)/..
 fi
 
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.pastebinit ] && ln -s ansible-pastebinit $rolesdir/juju4.pastebinit
+[ ! -e $rolesdir/juju4.pastebinit ] && cp -R $rolesdir/ansible-pastebinit $rolesdir/juju4.pastebinit
+
+## don't stop build on this script return code
+true
 
